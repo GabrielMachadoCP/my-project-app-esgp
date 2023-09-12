58af33a6298e31bdc131f25eee226d9567194ea0
GabrielMachadoP <gabrielmapi@gmail.com>
2023-09-12 11:21:15 -0300

    Adicionando o meu projeto no github

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+//rfc + enter
+
+import Cabecalho from "./components/Cabecalho";
+import Conteudo from "./components/Conteudo";
+import Rodape from "./components/Rodape";
+import reactLogo from "./assets/react.svg";
+import { Outlet } from "react-router-dom";
+
+export default function App() {
+
+  let reactLogoAlt = "React Logo";
+
+  //Lista de links redes sociais
+  let listaLinks =[<li>Github</li>, <li>Twitter</li>, <li>Reddit</li>];
+
+  return (
+    <>
+      <div className="container">
+
+        <Cabecalho/>
+
+        {/* Como estamos lidando com um conteúdo dinâmico, usamos o {} */}
+        {/* <img src={reactLogo} alt={reactLogoAlt}/> */}
+
+        {/* <Conteudo reactLogoProps={reactLogo} reactLogoAltProps={reactLogoAlt}/> */}
+
+        <Outlet/>
+
+        <Rodape listaProps={listaLinks}/>
+
+      </div>
+    </>
+  )
+}
