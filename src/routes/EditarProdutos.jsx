25a8a5c0cdbcd11544635b203be398b830c8113d
GabrielMachadoCP <rm99880@fiap.com.br>
2023-10-17 11:36:42 -0300

    Aula dia 17 - Modal, handleSubmit, handleChange...

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function EditarProdutos() {
 
   //Recuperando o parêmetro ID com o HOOK useParams();
   document.title = "EDITAR PRODUTOS ";
   const {id} = useParams();
-
+  const navigate = useNavigate();
   const [produto, setProduto] = useState({
     id: id,
     nome:'',
@@ -21,28 +21,54 @@ export default function EditarProdutos() {
     .catch(error=>console.log(error))
   }, [id])
 
+  const handleChange = (e)=>{
+
+    //Destructuring
+    const {name, value} = e.target;
+    //Preencher o useState com a função set... Utilizando o operador SPREAD...
+    setProduto({...produto, [name]:value});
+
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    fetch(`http://localhost:5000/produtos/${id}`, {
+      method: "PUT",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(produto)
+    })
+    .then(response => console.log("STATUS DA REQUISIÇÃO : " + response.status))
+    .catch(error=> console.log(error))
+    
+    //Redirect
+    navigate("/produtos")
+  }
+
+
   return (
     <div>
       <h1>EDITAR PRODUTOS</h1>
 
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <fieldset>
             <legend>Produto Selecionado</legend>
             
             <div>
               <label htmlFor="idNome">Nome</label>
-              <input type="text" name="nome" id="idNome" placeholder="Digite o nome do produto: " value={produto.nome} onChange={(e)=> setProduto(e.target.value)}/>
+              <input type="text" name="nome" id="idNome" placeholder="Digite o nome do produto: " value={produto.nome} onChange={handleChange}/>
             </div>
             
             <div>
               <label htmlFor="idDesc">Descrição</label>
-              <input type="text" name="desc" id="idDesc" placeholder="Digite a descrição do produto: " value={produto.desc} onChange={(e)=> setProduto(e.target.value)}/>
+              <input type="text" name="desc" id="idDesc" placeholder="Digite a descrição do produto: " value={produto.desc} onChange={handleChange}/>
             </div>
             
             <div>
               <label htmlFor="idPreco">Valor</label>
-              <input type="number" name="preco" id="idPreco" placeholder="Digite o valor do produto: " value={produto.preco} onChange={(e)=> setProduto(e.target.value)}/>
+              <input type="number" name="preco" id="idPreco" placeholder="Digite o valor do produto: " value={produto.preco} onChange={handleChange}/>
             </div>
             
             <div>
