70097d5be803377b037751a959e7b6eefe1383db
GabrielMachadoCP <rm99880@fiap.com.br>
2023-11-14 11:03:23 -0300

    Fazendo a autentificação

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -6,7 +6,8 @@ export default function Cabecalho() {
 
   const rotaAtual = useLocation();
 
-  return (
+  if(sessionStorage.getItem("token-user")){
+    return (
     <>
         <header className="cabecalho">
           
@@ -26,5 +27,28 @@ export default function Cabecalho() {
         </header> 
     </>
   )
+  } else{
+    return (
+      <>
+          <header className="cabecalho">
+            
+            <img src="/img/produtos.png" alt="Mãos segurando caixas." /> 
+  
+            {/* Crie uma lista com 5 links para as nossas rotas:
+            Obs: Utilize o componente Link do router-dom */}
+  
+            <nav>
+              <ul>
+                <li><Link to="/" className={rotaAtual.pathname == "/" ? "active" : ""}>HOME</Link></li>
+                <li><Link to="/login" className={rotaAtual.pathname == "/login" ? "active" : ""}>LOGIN</Link> </li>
+              </ul>
+            </nav>
+  
+          </header> 
+      </>
+    )
+  }
+
+  
 }
 
